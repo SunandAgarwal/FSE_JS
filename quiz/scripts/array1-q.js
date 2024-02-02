253967e50d2b7e8cc3751cab37d8c7fedaeb6bfb
Sunand Agarwal <sunandagarwal@Sunands-MacBook-Air.local>
2024-02-02 16:56:56 -0500

    fixes

--- quiz/scripts/array1-q.js
+++ quiz/scripts/array1-q.js
@@ -13,15 +13,22 @@ window.onload = function() {
               // empty the list so that we don't display duplicate entries
               // the display is regenerated every time a search term is entered.
               list.innerHTML = '';
+              myHistory.unshift(inp.value);
 
+              let myHistoryCopy = Array.from(myHistory);
+              myHistoryCopy.sort((a,b) => {
+                return a.length - b.length;
+            });
               // loop through the sorted array, and display all the search terms in the list
               for (const itemText of myHistoryCopy) {
-                
+                const liItem = document.createElement('li');
+                liItem.textContent = itemText;
+                list.appendChild(liItem);
               }
 
               // If the array length is 5 or more, remove the oldest search term
               if (myHistory.length >= MAX_HISTORY) {
-                
+                myHistory.pop();
               }
 
               // empty the search input and focus it, ready for the next term to be entered
